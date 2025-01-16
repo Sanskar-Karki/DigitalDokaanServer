e40bc20f4fc3f97200353ec140a40dea64568083
Sans <sanskarkarki20@gmail.com>
2025-01-16 10:54:59 +0545

    Table relation buildup

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,11 @@
-import { Sequelize } from "sequelize-typescript";
+import { ForeignKey, Sequelize } from "sequelize-typescript";
 import { envConfig } from "../config/config";
 import Product from "./models/productModel";
 import Category from "./models/categoryModel";
+import User from "./models/userModel";
+import Order from "./models/orderModel";
+import Payment from "./models/paymentModel";
+import OrderDetails from "./models/orderDetailsModel";
 
 const sequelize = new Sequelize(envConfig.connectionString as string, {
   models: [__dirname + "/models"],
@@ -24,6 +28,26 @@ try {
 // product table ma category id
 Product.belongsTo(Category, { foreignKey: "categoryId" });
 Category.hasOne(Product, { foreignKey: "categoryId" });
+
+// User X Orders
+// order table ma userid ,foreignKey chiyo
+Order.belongsTo(User, { foreignKey: "userId" });
+User.hasMany(Order, { foreignKey: "userId" });
+
+// Payment X Order
+// payment table ma orderId foreignkey chiyo
+Payment.belongsTo(Order, { foreignKey: "orderId" });
+Order.hasOne(Payment, { foreignKey: "orderId" });
+
+// Order X OrderDetails
+// OrderDetails ma orderId foreign key chiyo
+OrderDetails.belongsTo(Order, { foreignKey: "orderId" });
+Order.hasOne(OrderDetails, { foreignKey: "orderId" });
+
+// Product X OrderDetails
+// OrderDetails ma productId foreign key chiyo
+OrderDetails.belongsTo(Product, { foreignKey: "productId" });
+Product.hasOne(OrderDetails, { foreignKey: "productId" });
 
 // database synchronization
 sequelize.sync({ force: false, alter: false }).then(() => {
